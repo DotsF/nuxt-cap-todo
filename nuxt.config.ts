a8331b2d4de3fadecb592b0b9aaba53a99d946e0
teller <nov17hk@gmail.com>
2025-07-11 11:31:27 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    prerender: {
+      routes: ['/'],
+    },
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true }
 })
